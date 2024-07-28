0498601965b5be30629617bd5a6dbf9fe455c836
KevsterAmp <kevinamparado13@gmail.com>
2024-07-28 21:30:46 +0800

    remove stylua formatter

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
-				lua = { "stylua" },
+				-- lua = { "stylua" },
 				python = { "isort", "black" },
 				javascript = { "prettier" },
 				typescript = { "prettier" },
