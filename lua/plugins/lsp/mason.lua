dc674487d222cf62b8bcbd2159f693588d9da48f
KevsterAmp <kevinamparado13@gmail.com>
2024-07-28 21:35:31 +0800

    remove tailwind

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,44 +1,43 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		local mason = require("mason")
-		local mason_lspconfig = require("mason-lspconfig")
-		local mason_tool_installer = require("mason-tool-installer")
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+        local mason = require("mason")
+        local mason_lspconfig = require("mason-lspconfig")
+        local mason_tool_installer = require("mason-tool-installer")
 
-		mason.setup({
-			-- and opts here
-		})
+        mason.setup({
+            -- and opts here
+        })
 
-		mason_lspconfig.setup({
-			ensure_installed = {
-				"tsserver",
-				"html",
-				"cssls",
-				"tailwind",
-				"svelte",
-				"lua_ls",
-				"pyright",
-				"bashls",
-				"clangd",
-				"perlnavigator",
-				"dockerls",
-				"docker_compose_language_service",
-			},
-		})
+        mason_lspconfig.setup({
+            ensure_installed = {
+                "tsserver",
+                "html",
+                "cssls",
+                "svelte",
+                "lua_ls",
+                "pyright",
+                "bashls",
+                "clangd",
+                "perlnavigator",
+                "dockerls",
+                "docker_compose_language_service",
+            },
+        })
 
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"prettier",
-				"stylua",
-				"isort",
-				"black",
-				"eslint_d",
-				"pylint",
-			},
-		})
-	end,
+        mason_tool_installer.setup({
+            ensure_installed = {
+                "prettier",
+                "stylua",
+                "isort",
+                "black",
+                "eslint_d",
+                "pylint",
+            },
+        })
+    end,
 }
